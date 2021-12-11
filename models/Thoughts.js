6c83899fa8e7b2bf765396104dda85cf9efcece2
Ernesto Valecillos <evalecillos@gmail.com>
2021-12-10 21:41:09 -0600

    adding the apis. user seems done

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -2,34 +2,6 @@
 const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
-const ThoughtsSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 280,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAtVal) =>
-                dateFormat(createdAtVal)
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        reactions: [ReactionsSchema],
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true,
-        },
-        id: false,
-    }
-);
 
 const ReactionsSchema = new Schema(
     {
@@ -56,6 +28,36 @@ const ReactionsSchema = new Schema(
         toJSON: {
             getters: true,
         },
+    }
+);
+
+
+const ThoughtsSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) =>
+                dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [ReactionsSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
     }
 );
 
